55c3f4b2f9fc6bfe8a5096b53da94b583c65646e
Kyle Loveless <kjloveless321@gmail.com>
2024-02-25 18:08:31 -0600

    Add basic char counter.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+def letter_count(file_contents):
+  lower_file_contents = file_contents.lower()
+  result = {}
+  for char in lower_file_contents:
+    if char in result:
+      result[char] += 1
+    else:
+      result[char] = 1
+  print(result)
+
 def num_words(file_contents):
   words = file_contents.split()
   print(len(words))
@@ -5,7 +15,8 @@ def num_words(file_contents):
 def main():
   with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+    # print(file_contents)
     num_words(file_contents)
+    letter_count(file_contents)
 
 main()
